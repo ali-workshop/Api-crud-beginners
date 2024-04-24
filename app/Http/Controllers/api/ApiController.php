84df883dec73205298a8788ade64b2a071c5ce53
ALI ALI <alirafikali22@gmail.com>
2024-04-24 12:22:47 +0300

    build the index

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
+
+
+        public function index(){
+    
+
+            $students=Student::all();
+
+            return response()->json($students);
+
+        }
 
 
 
