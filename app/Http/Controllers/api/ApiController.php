7730f1c0a9374c1aba4e83ad081ec01fae81b3da
ALI ALI <alirafikali22@gmail.com>
2024-04-25 11:34:20 +0300

    add the update api

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -54,5 +54,52 @@ class ApiController extends Controller
         }
 
 
+        public function udpate (Request $request,$id){
+            $validator=Validator::make($request->all(),[
+                'name' => 'string',
+                'age' => 'integer'
 
+            ]);
+            if($validator->fails()){
+
+
+                $data=[
+                    'status'=>'422',
+                    'message' => $validator->messages()
+
+                ];
+                return response()->json($data,422);
+
+
+
+
+
+
+            }
+
+            $old_student_data=Student::find($id);
+            $new_student_info=$old_student_data->update([
+                'name'=>$request->name,
+                'age'=>$request->age
+
+
+            ]);
+            // $old_student_data->name=$request->name;
+            // $old_student_data->age=$request->age;
+            // $old_student_data->save();
+            $data=[
+                
+                'status'=>'200',
+                'old_student_info' => $old_student_data,
+                'message'=>"the studet information updated successfully",
+                'student'=>$new_student_info
+
+
+            ];
+            return response()->json($data,200);
+
+
+        } 
+
+        
 }
