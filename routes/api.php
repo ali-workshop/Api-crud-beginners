796439aaf9ed5c676e8dfd591251af11d693ed4b
ALI ALI <alirafikali22@gmail.com>
2024-04-26 11:03:19 +0300

    fix bugs

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::get('/user', function (Request $request) {
 Route::get('/student', [ApiController::class,'index']);
 Route::post('/student', [ApiController::class,'store']);
 Route::put('/student/{id}', [ApiController::class,'update']);#see the category id case ali saleh 24/4 when use the www-xxx to fix it aram. 
+Route::delete('/student/{id}', [ApiController::class,'delete']);
